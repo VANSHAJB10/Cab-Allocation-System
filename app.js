e349b1ae66e9b25f11db46d2d2b9ec21616998d4
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2024-10-30 18:08:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 
+// handling errors of strict MIME checking in browser
+app.use(express.static('public')); 
 
 const path = require("path");
-const http = require("http");
-const socketio = require("socket.io");
 
+const http = require("http");// package in node
+const socketio = require("socket.io");
 const server = http.createServer(app);
 
 const io = socketio(server);
@@ -13,11 +15,21 @@ const io = socketio(server);
 app.set("view engine", "ejs");
 app.set(express.static(path.join(__dirname, "public")));
 
+//handling connection request from socket.io
 io.on("connection", function(socket){
+    socket.on("send-location", function(data){
+    io.emit("recieve-location",{id: socket.id, ...data});
+    });
+
+    socket.on("disconnet", function(){
+    io.emit("user-disconnected", socket.id);
+    });
+    
     console.log("connected");
 });
 
 app.get("/", function(req,res){
     res.render("index");
 });
+
 server.listen(3000);
